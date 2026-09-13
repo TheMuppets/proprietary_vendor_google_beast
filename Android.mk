bac416a660cef37efa24ceef6c7d4c670aac377b
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-13 15:09:43 -0400

    Pin padded bootloader
    Change-Id: I61ac10af0be3f7a1430eaed68d173f146c076832

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),beast)
 
-$(call add-radio-file-sha1-checked,radio/bootloader.img,99b0aa056b1cae376f6676734dbc6d94aac5c534)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,fd84f98e954fffb533e5bf9f1ebb1f4ad366659d)
 
 endif
